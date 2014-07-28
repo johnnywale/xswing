df371df433f084fa9d79a4a81629eae9ca874c83
johnny <johnny@192.168.48.132>
2014-07-28 13:28:18 +0800

    fix conflict

---
+++ src/main/java/com/jovx/swing/event/BaseModelSizeChangeEvent.java
@@ -0,0 +1,53 @@
+package com.jovx.swing.event;
+
+import java.util.List;
+
+public class BaseModelSizeChangeEvent<T> implements ModelListener<T> {
+	private List<T> originalList;
+	private List<T> changedList;
+	private List<T> currentList;
+	private Class<T> instanceClass;
+
+	public List<T> getChangedList() {
+		return changedList;
+	}
+
+	public void setChangedList(List<T> changedList) {
+		this.changedList = changedList;
+	}
+
+	public List<T> getOriginalList() {
+		return originalList;
+	}
+
+	public void setOriginalList(List<T> originalList) {
+		this.originalList = originalList;
+	}
+
+	public List<T> getCurrentList() {
+		return currentList;
+	}
+
+	public void setCurrentList(List<T> currentList) {
+		this.currentList = currentList;
+	}
+
+	@Override
+	public String toString() {
+		return getClass().getSimpleName() + " [originalList="
+				+ originalList.size() + ", changedList=" + changedList.size()
+				+ ", currentList=" + currentList.size() + "]";
+	}
+
+	@Override
+	public Class<T> getInstanceClass() {
+
+		return instanceClass;
+	}
+
+	public void setInstanceClass(Class<T> instanceClass) {
+		this.instanceClass = instanceClass;
+
+	}
+
+}
